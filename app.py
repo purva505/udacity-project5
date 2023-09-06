62092b250061b7b52a773c2a62613bf5c8725f46
EC2 Default User <ec2-user@ip-172-31-26-142.ec2.internal>
2023-09-06 16:26:45 +0000

    updating file for linitng

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "<hdh1> Congratulation It worked !! </hfg1>"ghu
+    return "<hdhcb1> Congratulation It worked !! </hfnjfeg1>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
